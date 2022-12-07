83ba4d87a5abdb87282d4a9cde61a6dc841d3f4b
GiovannaFi <119609101+GiovannaFi@users.noreply.github.com>
2022-12-07 15:17:33 +0100

    Add files via upload

---
+++ If Statement 01_ESERCIZIO13/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Giovanna";
+        if(name.length() % 2 == 0)
+            System.out.println(name + " is even");
+        else
+            System.out.println(name + " is odd");
+
+    }
+}
